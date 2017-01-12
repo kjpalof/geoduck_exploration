46c9ee3a3296e8cc38dfafc8a9eba255baa4e7d5
Katie Palof <katie.palof@alaska.gov>
2017-01-11 16:15:36 -0900

    initial setup for k-s tests between areas.

---
+++ code/k_s_test.R
@@ -0,0 +1,21 @@
+#K.Palof 
+# Geoduck age exploration - addressing issues from late 2016/early 2017
+# see read me for more
+
+#Testing for samples from the same distribution 
+# K-S test?
+
+rm(list = ls()) # clear workspace since data frames have same names
+
+## Load packages ------
+library(tidyverse)
+
+## Load data -------
+dat <- read.csv("./data/12_14_geoduck_all.csv")
+
+dat %>%
+  filter(Fishery.Area == "1_Cone Is.") -> cone_is
+dat %>%
+  filter(Fishery.Area == "2_East San Fernando") ->sanfran
+# K-S test
+ks.test(cone_is$Age_2012, sanfran$Age_2012)

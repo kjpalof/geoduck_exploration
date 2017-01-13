ebb0804f63cdf4d2144d99b740bb2139bde3f2e0
Katie Palof <katie.palof@alaska.gov>
2017-01-12 16:10:49 -0900

    additions to this type of testing with A-D test

--- code/k_s_test.R
+++ code/k_s_test.R
@@ -9,6 +9,7 @@ rm(list = ls()) # clear workspace since data frames have same names
 
 ## Load packages ------
 library(tidyverse)
+library(kSamples)
 
 ## Load data -------
 dat <- read.csv("./data/12_14_geoduck_all.csv")
@@ -17,5 +18,13 @@ dat %>%
   filter(Fishery.Area == "1_Cone Is.") -> cone_is
 dat %>%
   filter(Fishery.Area == "2_East San Fernando") ->sanfran
+
+
+
 # K-S test
 ks.test(cone_is$Age_2012, sanfran$Age_2012)
+
+
+#A-D test, k sample tests
+ad.test(Age_2012 ~ Fishery.Area, data = dat)
+# reject null hypothosis that all samples come from the same distribution.

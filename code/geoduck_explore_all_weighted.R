cdcf4503b292fdc0e4cac7127475314094b6fdb5
Katie Palof <katie.palof@alaska.gov>
2017-02-01 09:38:32 -0900

    work on figures. Need to determine how to add weightings - can I do this in histogram?

--- code/geoduck_explore_all_weighted.R
+++ code/geoduck_explore_all_weighted.R
@@ -27,9 +27,14 @@ dat %>% group_by(ADFG_Fishery.Area, Age_2012) %>%
 ggplot(dat, aes(x = Age_2012)) +geom_histogram(binwidth =1.5) #all together, no weighting
 ggplot(dat, aes(x=Age_2012)) + 
   geom_histogram(aes(y=..density..),      # Histogram with density instead of count on y-axis
-                 binwidth=1.5,
+                 binwidth=1.0,
                  colour="black", fill="white") +
   geom_density(alpha=.2, fill="#FF6666")  # Overlay with transparent density plot
+
+ggplot(dat, aes(x=Age_2012, fill=ADFG_Fishery.Area)) + 
+  geom_histogram(aes(y=..density..),      # Histogram with density instead of count on y-axis
+                 binwidth=1.5) +
+  geom_density()  # Overlay with transparent density plot
 
 ggplot(dat, aes(x=Age_2012, fill=ADFG_Fishery.Area)) +
   geom_histogram(binwidth=1.5, alpha=.5, position="identity")
@@ -38,8 +43,20 @@ ggplot(dat, aes(x=Age_2012, fill=ADFG_Fishery.Area)) + geom_density(alpha=.3)
 ggplot(dat, aes(x=Age_2012)) + geom_histogram(binwidth=1.5, colour="black", fill="white") + 
   facet_grid( ADFG_Fishery.Area~ .)
 
-### histograms from weighted data??? how to do this
+### histograms from summarized data -------------------
 dat_hist
+ggplot(dat_hist, aes(x=Age_2012, y = n))  + 
+  geom_bar(stat = "identity", width =0.5) +geom_density()
+
+
+ggplot(dat_hist, aes(x=Age_2012, y = n, fill = ADFG_Fishery.Area))  + 
+  geom_bar(stat = "identity", width =0.5)
+ggplot(dat_hist, aes(x=Age_2012, y = n, fill = ADFG_Fishery.Area))  + 
+  geom_bar(stat="identity")+
+  scale_fill_brewer(palette="Paired")+
+  theme_minimal()
+
+
 
 # match area names to merge files.
 # Calculate weighting - compare to Mike's calc - see variable in file

34133bf08600135539b69a1558bcd29a902731fd
Katie Palof <katie.palof@alaska.gov>
2017-01-31 16:27:47 -0900

    work on comparisons between survey area distributions

--- code/k_s_test.R
+++ code/k_s_test.R
@@ -18,11 +18,33 @@ dat %>%
   filter(Fishery.Area == "1_Cone Is.") -> cone_is
 dat %>%
   filter(Fishery.Area == "2_East San Fernando") ->sanfran
-
+dat %>%
+  filter(Fishery.Area == "3_Nakat Inlet") ->nakat
+dat %>%
+  filter(Fishery.Area == "4_Vallenar Bay") ->val
+dat %>%
+  filter(Fishery.Area == "5_Vegas/Hotspur Is.") ->vegh
+dat %>%
+  filter(Fishery.Area == "6_Tlevak Strait") -> tlev
+dat %>%
+  filter(Fishery.Area == "7_Warren/Kosciusko Is.") -> warren
+dat %>%
+  filter(Fishery.Area == "8_Taigud/Kolosh Is.") ->tkis
+dat %>%
+  filter(Fishery.Area == "9_Biorka/Legma Is.") ->biork
 
 
 # K-S test
-ks.test(cone_is$Age_2012, sanfran$Age_2012)
+ks.test(cone_is$Age_2012, sanfran$Age_2012) # reject null
+ks.test(cone_is$Age_2012, nakat$Age_2012) # reject null
+ks.test(cone_is$Age_2012, val$Age_2012) # reject null
+ks.test(cone_is$Age_2012, vegh$Age_2012) # reject null
+ks.test(cone_is$Age_2012, tlev$Age_2012) # reject null
+ks.test(cone_is$Age_2012, warren$Age_2012) # reject null
+ks.test(cone_is$Age_2012, tkis$Age_2012) # reject null
+ks.test(cone_is$Age_2012, biork$Age_2012) # reject null
+
+
 
 
 #A-D test, k sample tests

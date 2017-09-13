140070056be337acd30de579185ee3cfbcfcdbdd
Katie Palof <katie.palof@alaska.gov>
2017-09-13 15:04:56 -0800

    edits to summarizing data initially

--- code/regional_age_growth_cc.R
+++ code/regional_age_growth_cc.R
@@ -43,12 +43,12 @@ dat %>% mutate(area1 = as.character(ADFG_Fishery.Area))  %>%
 # full data set with weigtings
 dat1 %>% left_join(weight_pop) -> dat2 # raw data
 # summarized by area, age with weigthing
-dat2 %>% group_by(ADFG_Fishery.Area, Age_2012) %>% 
-  summarise(n = n())  -> dat2_by.area
+dat2 %>% group_by(area, Age_2012) %>% 
+  summarise(n = n())  -> dat2_by.area # counts by age for each area 
 dat2_by.area %>% left_join(weight_pop) %>% mutate(n_wt = n*wt_each) -> dat_wt_by.area # counts by age/area with weighting
 
 ## weighting counts 
-dat %>% group_by(ADFG_Fishery.Area) %>% summarise(N_samp = n()) ->total.n.area  # number sampled in each area
+dat1 %>% group_by(area) %>% summarise(N_samp = n()) ->total.n.area  # number sampled in each area
 dat_wt_by.area %>% left_join(total.n.area) %>% 
   mutate(prop = n/N_samp, n_corrected = prop*popsize_wshow) -> dat_wt_by.area2
 # n_corrected is counts using population size weighting.  n_wt is proportion using same weighting
